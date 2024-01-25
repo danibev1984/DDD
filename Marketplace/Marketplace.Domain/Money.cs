5d706818a0b5f6c2186ce92f90c670a663ef25e4
danibev1984 <danielebevilacqua1984@gmail.com>
2024-01-25 15:12:45 +0100

    adding Money class (test will fail)

---
+++ Marketplace/Marketplace.Domain/Money.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Marketplace.Domain
+{
+    public class Money
+    {
+        public decimal Amount { get; }
+        public Money(decimal amount) => Amount = amount;    
+    }
+}
