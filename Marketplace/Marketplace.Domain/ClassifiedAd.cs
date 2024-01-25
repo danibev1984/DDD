71bd257a92ef539e35f5b70f329dc521b52eff79
danibev1984 <danielebevilacqua1984@gmail.com>
2024-01-25 14:53:15 +0100

    first naif implementation of ClassifiedAd

---
+++ Marketplace/Marketplace.Domain/ClassifiedAd.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Marketplace.Domain
+{
+    public class ClassifiedAd
+    {
+        private Guid _ownerId;
+        private string _title;
+        private string _text;
+        private decimal _price;
+        
+        public Guid Id { get; }
+
+        public ClassifiedAd(Guid id, Guid ownerId)
+        {
+            if (id == default)
+                throw new ArgumentNullException("identity must be specified", nameof(id));
+
+            if (ownerId == default)
+                throw new ArgumentException("owner id must be specified", nameof(ownerId));
+
+            Id = id;
+            _ownerId = ownerId;
+        }
+
+        public void SetTitle(string title) => _title = title;
+        public void UpdateText(string text) => _text = text;
+        public void UpdatePrice(decimal price) => _price = price;
+    }
+}
